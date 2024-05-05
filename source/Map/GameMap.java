7e5124b410beee8684d20478499173a81438c392
TjapDjae77 <r.f.rayhan@gmail.com>
2024-05-05 18:30:13 +0700

    Updating GameMap

--- source/Map/GameMap.java
+++ source/Map/GameMap.java
@@ -1,31 +1,56 @@
-package source.Map;
+import javax.swing.*;
+import java.awt.*;
 
-public class GameMap {
+public class GameMap extends JPanel {
     private final int width = 9;
     private final int height = 6;
-    private final Tiles[][] grid;
+    private Tiles[][] grid;
     
     public GameMap() {
-        grid = new Tiles[width][height];
-        initializeMap();
-    }
-
-    public void initializeMap(){
-        for (int j = 0; j < height; j++) {
-            grid[0][j] = new ProtectedTiles();
-        }
-        for (int i = 1; i < width-1; i++){
-            for(int j = 0; j < height; j++){
-                if((j == 2) || (j == 3)){
+        grid = new Tiles[height][width];
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++){
+                if(j == 0){
+                    grid[i][j] = new ProtectedTiles();
+                }
+                else if(j == width-1){
+                    grid[i][j] = new SpawnTiles();
+                }
+                else if((i == 2) || (i == 3)){
                     grid[i][j] = new PoolTiles();
                 }
                 else{
-                    grid[i][j] = new LandTiles();
+                    grid[i][j] = new GrassTiles();
                 }
             }
         }
-        for(int j = 0; j < height; j++){
-            grid[8][j] = new SpawnTiles();
-;        }
+    }
+    
+    public Tiles getTile(int row, int column){
+        if (row >= 0 && row < grid.length && column >= 0 && column < grid[0].length) {
+            return grid[row][column];
+        } else {
+            // Jika indeks yang diminta di luar rentang grid, throw Exception
+            throw new IllegalArgumentException("Invalid row or column index");
+        }
+    }
+
+    public int getHeight(){
+        return height;
+    }
+    
+    public int getWidth(){
+        return width;
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int tileSize = 100; // Ukuran petak yang diinginkan (100x100)
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                // Menggambar petak pada koordinat (j*tileSize, i*tileSize)
+                grid[i][j].draw(g, j * tileSize, i * tileSize, tileSize, tileSize);
+            }
+        }
     }
 }
