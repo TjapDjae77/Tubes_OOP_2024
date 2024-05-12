dfaa047586e0ed9bfa3f1dbc8b2a9621b85daf7d
Tim-Buktu <82936135+Tim-Buktu@users.noreply.github.com>
2024-05-13 01:35:06 +0700

    Feat : add inventory.java

--- source/Inventory/inventory.java
+++ source/Inventory/inventory.java
@@ -1,1 +1,60 @@
 package source.Inventory;
+import source.Characters.Plants.Plants;
+public class inventory {
+    public static int tanamanInDeck=0;
+    private Plants selectedPlant;
+    private Plants[] gameDeck;
+    private Plants[] inventoryList;
+
+    public inventory(){
+        this.selectedPlant = null;
+        this.gameDeck = new Plants[6];
+    }
+
+    public void selectPlant(Plants plant, int index){
+        //LIST OF PLANTS
+        if (gameDeck[index] == null){
+            gameDeck[index] = plant;
+            tanamanInDeck++;
+        }
+        else{
+            Plants temp = gameDeck[index];
+            gameDeck[index] = plant;
+            gameDeck[tanamanInDeck] = temp;
+            this.selectedPlant = plant;
+        }
+    }
+    public void switchPlants(int index1, int index2, int category){
+        if (index1 == index2){
+            return;
+        }
+        //Tuker inventory
+        if (category == 1){
+            //Empty slot
+            if (inventoryList[index1] == null || inventoryList[index2] == null || index1>6 || index2>6){
+                return;
+            }
+            //Tuker
+            Plants temp = inventoryList[index1];
+            inventoryList[index1] = inventoryList[index2];
+            inventoryList[index2] = temp;
+        }
+        //Tuker gameDeck
+        else{
+            //Empty slot
+            if (gameDeck[index1] == null || gameDeck[index2] == null || index1>6 || index2>6){
+                return;
+            }
+            //Tuker
+            Plants temp = gameDeck[index1];
+            gameDeck[index1] = gameDeck[index2];
+            gameDeck[index2] = temp;
+        }
+    }
+    public void removePlant(int index){
+        if (gameDeck[index] != null && index<6){
+            gameDeck[index] = null;
+            tanamanInDeck--;
+        }
+    }
+}
