30736ed96a313906f472f52bc316b0b40e8ad7c0
JeremyDeandito <deanditojeremy@gmail.com>
2024-05-13 17:44:04 +0700

    update sun

--- source/Sun/sun.java
+++ source/Sun/sun.java
@@ -1,1 +1,28 @@
-package source.Sun;
+//package source.Sun;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Random;
+
+public class Sun{
+    private int sun = 25;
+    Timer timer = new Timer();
+    
+    public void addSun(){
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run(){
+                sun += 25;
+                System.out.println("Sun saat ini : " + sun);
+                addSun();
+            }
+        }, randomTime(5, 10)*1000);
+        
+    }
+
+    public int randomTime(int min, int max){
+        Random random = new Random();
+        int randomTime = (random.nextInt((max - min)+1)+ min);
+        return randomTime;
+    }
+}
