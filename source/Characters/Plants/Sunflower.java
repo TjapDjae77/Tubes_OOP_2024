f727550e5b3bab88180e93e871a0ff5c29001f76
faamahardika <116859674+faamahardika@users.noreply.github.com>
2024-05-14 23:28:07 +0700

    nambahin fungsi sun di sunflower + demo

--- source/Characters/Plants/Sunflower.java
+++ source/Characters/Plants/Sunflower.java
@@ -1,6 +1,25 @@
 package source.Characters.Plants;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Sunflower extends Plants {
-    public Sunflower(String name, int health, int damage, int attack_speed, int[] position, int cost, int range, int cooldown) {
+    private int sun = 25;
+    Timer timer = new Timer();
+
+    public Sunflower(String name, int health, int damage, int attack_speed, int[] position, int cost, int range, int cooldown, int sun) {
         super(name, 100, 0, 0, position, 50, 0, 10);
+        this.sun = sun; 
+    }
+
+    public void addSunSunflower(){
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run(){
+                sun += 25;
+                System.out.println("Sun saat ini : " + sun);
+                addSunSunflower();
+            }
+        }, 3000);
     }
 }
---
+++ source/Characters/Plants/SunflowerDemo.java
@@ -0,0 +1,10 @@
+package source.Characters.Plants;
+
+public class SunflowerDemo {
+    public static void main(String[] args) {
+        int[] position = {0, 0}; // Example position, adjust as needed
+        Sunflower sunflower = new Sunflower("Sunflower", 100, 0, 0, position, 50, 0, 10, 25);
+        sunflower.addSunSunflower();
+    }
+}
+
