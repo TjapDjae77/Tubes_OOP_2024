06651454e83739517f414e829e0e62929c3feae2
Tim-Buktu <82936135+Tim-Buktu@users.noreply.github.com>
2024-05-16 00:29:34 +0700

    revise menu

--- source/Menu/Menu.java
+++ source/Menu/Menu.java
@@ -1,11 +1,23 @@
 package source.Menu;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import source.Inventory.Inventory;
+import source.Characters.Zombie.BucketheadZombie;
+import source.Characters.Zombie.ConeheadZombie;
+import source.Characters.Zombie.DolphinRiderZombie;
+import source.Characters.Zombie.DuckyTubeZombie;
+import source.Characters.Zombie.FootballZombie;
+import source.Characters.Zombie.JackInTheBoxZombie;
+import source.Characters.Zombie.NewspaperZombie;
+import source.Characters.Zombie.NormalZombie;
+import source.Characters.Zombie.PoleVaultingZombie;
+import source.Characters.Zombie.ScreenDoorZombie;
 import source.Characters.Zombie.Zombie;
 import source.Characters.Plants.Plants; // Import the Plants class
 
 public class Menu {
+    public static ArrayList<Zombie> zombieList;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int choice;
@@ -23,11 +35,21 @@ public class Menu {
 
             switch (choice) {
                 case 1:
-
                     // Call the method to start the game
                     // You might need to setup the deck of plants here
                     break;
                 case 2:
+                    System.out.println("\n*** Game Instructions ***");
+                    System.out.println("1. Choose plants from the inventory to build your deck.");
+                    System.out.println("2. Start the game and place your plants strategically to defend your house.");
+                    System.out.println("3. Survive the zombie attacks and win the game!");
+                    System.out.println("\n*** Command List ***");
+                    System.out.println("1. Select plant from inventory: select <plant_name>");
+                    System.out.println("2. Place plant on the grid: place <plant_name> <row> <column>");
+                    System.out.println("3. Remove plant from the grid: remove <row> <column>");
+                    System.out.println("4. Show the grid: show grid");
+                    System.out.println("5. Show inventory: show inventory");
+                    System.out.println("6. End turn: end turn");
                     // Call the method to show help
                     // This should include game instructions and command list
                     // ...
@@ -41,9 +63,30 @@ public class Menu {
                     // This should include all attributes of the plants
                     break;
                 case 4:
+                    BucketheadZombie bucketheadZombie = new BucketheadZombie();
+                    ConeheadZombie coneheadZombie = new ConeheadZombie();
+                    DolphinRiderZombie dolphinRiderZombie = new DolphinRiderZombie();
+                    DuckyTubeZombie duckyTubeZombie = new DuckyTubeZombie();
+                    FootballZombie footballZombie = new FootballZombie();
+                    JackInTheBoxZombie jackInTheBoxZombie = new JackInTheBoxZombie();
+                    NewspaperZombie newspaperZombie = new NewspaperZombie();
+                    NormalZombie normalZombie = new NormalZombie();
+                    PoleVaultingZombie poleVaultingZombie = new PoleVaultingZombie();
+                    ScreenDoorZombie screenDoorZombie = new ScreenDoorZombie();
+                    zombieList = new ArrayList<>();
+                    zombieList.add(bucketheadZombie);
+                    zombieList.add(coneheadZombie);
+                    zombieList.add(dolphinRiderZombie);
+                    zombieList.add(duckyTubeZombie);
+                    zombieList.add(footballZombie);
+                    zombieList.add(jackInTheBoxZombie);
+                    zombieList.add(newspaperZombie);
+                    zombieList.add(normalZombie);
+                    zombieList.add(poleVaultingZombie);
+                    zombieList.add(screenDoorZombie);
                     System.out.println("\n*** Zombies List ***");
                     for (Zombie zombie : zombieList) {
-                        printZombie(zombie);
+                        zombie.showDescription();
                     }
                     // Call the method to show the list of zombies
                     // This should include all attributes of the zombies
