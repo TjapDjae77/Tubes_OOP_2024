d5cb2dafd1ebc778b4b5687e2c419450a6ebac34
faamahardika <116859674+faamahardika@users.noreply.github.com>
2024-05-16 17:53:50 +0700

    revisi snopy

--- source/Characters/Plants/Snowpea.java
+++ source/Characters/Plants/Snowpea.java
@@ -31,7 +31,7 @@ public class Snowpea extends Plants{
 
     public void attack(Zombie zombie) {
         if (zombie != null) {
-            zombie.setHealth(this.attack_damage);
+            zombie.setHealth(zombie.getHealth()-this.attack_damage);
             System.out.println(this.name + " attacks " + zombie.getName() + " for " + this.attack_damage + " damage.");
             System.out.println(zombie.getName() + " has " + zombie.getHealth() + " health remaining.");
             applySlowEffect(zombie);
