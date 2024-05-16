7481f4a2d6589d1e2ac60942fae0813d364d463e
Tim-Buktu <82936135+Tim-Buktu@users.noreply.github.com>
2024-05-17 01:01:05 +0700

    benerin tiles

--- source/Deck/Deck.java
+++ source/Deck/Deck.java
@@ -29,7 +29,7 @@ public class Deck{
 
     }
 
-    public void digPlants(){
+    public void digPlants(int row, int column){
 
     }
 }
--- source/Map/Tiles.java
+++ source/Map/Tiles.java
@@ -1,27 +1,30 @@
-package source.Map;
-
+package source.Map.*;
+import source.Characters.Plants.*;
 import java.awt.*;
 
 public abstract class Tiles {
-    protected boolean unplanted;
     private String tilesType;
+    private Plants planted;
 
     public Tiles(String tilesType){
-        unplanted = false;
         this.tilesType = tilesType;
-    }
-
-    public boolean getStatus(){
-        return unplanted;
-    }
-
-    public void setStatus(boolean unplanted){
-        this.unplanted = unplanted;
+        this.planted = null;
     }
 
     public String getTilesType(){
         return tilesType;
     }
 
+    public Plants getPlanted(){
+        return planted;
+    }
+
+    public void setPlanted(Plants planted){
+        this.planted = planted;
+    }
+
+    public void deadPlanted(){
+        this.planted = null;
+    }
     public abstract void draw(Graphics g, int x, int y, int width, int height);
 }
