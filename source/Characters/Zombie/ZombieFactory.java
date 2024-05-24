a00f0dcca0bf586cd86e9f8521549029f7cd642d
Tim-Buktu <82936135+Tim-Buktu@users.noreply.github.com>
2024-05-24 20:23:29 +0700

    Update ZombieFactory.java

--- source/Characters/Zombie/ZombieFactory.java
+++ source/Characters/Zombie/ZombieFactory.java
@@ -7,29 +7,37 @@ public class ZombieFactory {
 
     public static Zombie createZombie(boolean aquatic){
         if(aquatic) {
-            int zombieType = ThreadLocalRandom.current().nextInt(1);
+            int zombieType = ThreadLocalRandom.current().nextInt(2);
             switch (zombieType) {
                 case 0:
                     return new DolphinRiderZombie();
 
                 case 1:
-                    return new DolphinRiderZombie();
+                    return new DuckyTubeZombie();
 
                 default:
                     return null;
             }
         }
         else {
-            int zombieType = ThreadLocalRandom.current().nextInt(1);
+            int zombieType = ThreadLocalRandom.current().nextInt(8);
             switch (zombieType) {
                 case 0:
                     return new PoleVaultingZombie();
-
                 case 1:
                     return new ConeheadZombie();
-
                 case 2:
                     return new BucketheadZombie();
+                case 3:
+                    return new ScreenDoorZombie();
+                case 4:
+                    return new NewspaperZombie();
+                case 5:
+                    return new FootballZombie();
+                case 6:
+                    return new NormalZombie();
+                case 7:
+                    return new JackInTheBoxZombie();
 
                 default:
                     return new DuckyTubeZombie();
